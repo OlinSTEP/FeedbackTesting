33dc2a49164fac7ffde38c8d6a303275ac683920
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-01-30 15:30:13 -0500

    Initial Commit

---
+++ Haptic Tester/Haptic_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_TesterApp.swift
+//  Haptic Tester
+//
+//  Created by Paul Ruvolo on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
